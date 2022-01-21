962501e6cce4245d71e3a99cff66d1d662c0c936
ymadd <Kb_yamato@protonmail.com>
2022-01-21 17:19:51 +0900

    Update main.go

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, Docker Actions")
+
+	firstGreeting := os.Getenv("INPUT_FIRST_GREETING")
+	secondGreeting := os.Getenv("INPUT_SECOND_GREETING")
+	thirdGreeting := os.Getenv("INPUT_THIRD_GREETING")
+
+	fmt.Println("Hello" + firstGreeting)
+	fmt.Println("Hello" + secondGreeting)
+
+	if thirdGreeting != "" {
+		fmt.Println("Hello" + thirdGreeting)
+	}
 }
